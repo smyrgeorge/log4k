b38c9b835d5059ce7735b6e90cd83a72bda8ead0
George Smyrnaios <smyrgeorge@gmail.com>
2024-12-01 14:30:15 +0100

    Refactor logging implementation for coroutines support
    Eliminated the `SimpleLoggingEvent` and `SimpleCoroutinesLoggingEvent` classes, consolidating logic into a unified `LoggingEvent` class. Introduced comprehensive coroutine-based examples and documentation, enhancing logging with coroutines in Kotlin multiplatform. Updated project version to 0.30.0 and included a new example demonstrating coroutine logging.

--- log4k/src/jvmMain/kotlin/io/github/smyrgeorge/log4k/impl/extensions/platform.jvm.kt
+++ log4k/src/jvmMain/kotlin/io/github/smyrgeorge/log4k/impl/extensions/platform.jvm.kt
@@ -1,15 +1,13 @@
 package io.github.smyrgeorge.log4k.impl.extensions
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.asCoroutineDispatcher
-import java.util.concurrent.Executors
+import kotlinx.coroutines.Dispatchers
 import kotlin.reflect.KClass
 
 actual fun KClass<*>.toName(): String =
     qualifiedName ?: simpleName ?: error("Could not extract the class-name of $this")
 
-private val dispatcher = Executors.newVirtualThreadPerTaskExecutor().asCoroutineDispatcher()
-actual fun dispatcher(): CoroutineDispatcher = dispatcher
+actual fun dispatcher(): CoroutineDispatcher = Dispatchers.IO
 actual fun thread(): String = Thread.currentThread().name
 actual fun threadId(): Int = Thread.currentThread().id.toInt()
 actual fun platformPrintlnError(message: String) = println(message)
