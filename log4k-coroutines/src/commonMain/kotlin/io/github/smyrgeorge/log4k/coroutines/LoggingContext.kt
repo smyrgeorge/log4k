2e5819086979c84a3bf59caec3d4d262617f2839
George Smyrnaios <smyrgeorge@gmail.com>
2024-12-01 15:05:47 +0100

    Refactor LoggingContext for enhanced tracing functionality
    Expanded LoggingContext constructors to accept various combinations of tags and spans. Improved documentation and added detailed comments for clarity and future maintainability. Simplified SpanStack class by removing unused methods and updated the span operation logic for better error handling.

--- log4k-coroutines/src/commonMain/kotlin/io/github/smyrgeorge/log4k/coroutines/LoggingContext.kt
+++ log4k-coroutines/src/commonMain/kotlin/io/github/smyrgeorge/log4k/coroutines/LoggingContext.kt
@@ -7,36 +7,57 @@ import io.github.smyrgeorge.log4k.impl.Tags
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.coroutineContext
 
-@Suppress("unused", "MemberVisibilityCanBePrivate")
-class LoggingContext(
-    val tags: Tags = emptyMap(),
-) : CoroutineContext.Element {
+/**
+ * Represents a logging context that is used within a coroutine for tracing and tagging operations.
+ * It functions as a coroutine context element, managing tags and a stack of spans.
+ * Provides constructors to initialize with different combinations of tags and a span.
+ */
+@Suppress("unused")
+class LoggingContext : CoroutineContext.Element {
+    val tags: Tags
     val spans: SpanStack = SpanStack()
 
-    constructor(span: Span) : this() {
+    constructor() {
+        tags = emptyMap()
+    }
+
+    constructor(tags: Tags) {
+        this.tags = tags
+    }
+
+    constructor(span: Span) {
+        tags = emptyMap()
         spans.push(span)
     }
 
-    constructor(tags: Tags, span: Span) : this(tags = tags) {
+    constructor(tags: Tags, span: Span) {
+        this.tags = tags
         spans.push(span)
     }
 
+    /**
+     * Executes a function within the scope of a tracing span.
+     *
+     * @param T The type of the result produced by the function.
+     * @param name The name of the span.
+     * @param f A function to be executed within the span context.
+     * @return The result produced by the function `f`.
+     */
     inline fun <T> span(name: String, f: Span.Local.() -> T): T {
         val current = spans.peek() ?: error("No span found!")
-        val span = current.context.tracer.span(name, current).also {
-            it.start()
+        val span = current.context.tracer.span(name, current).start().also {
             spans.push(it)
         }
         return try {
             f(span).also {
                 span.end()
-                spans.pop()
             }
         } catch (e: Throwable) {
             span.exception(e, true)
             span.end(e)
-            spans.pop()
             throw e
+        } finally {
+            spans.pop()
         }
     }
 
@@ -55,9 +76,22 @@ class LoggingContext(
         fun of(span: Span, tags: Tags): LoggingContext = LoggingContext(tags, span)
         fun of(span: Span, vararg tags: Tag): LoggingContext = LoggingContext(tags.toMap(), span)
 
+        /**
+         * Retrieves the current `LoggingContext` from the coroutine's context.
+         * If there is no `LoggingContext` present, it returns an empty context.
+         *
+         * @return The current `LoggingContext` if available, or an empty context if not.
+         */
         suspend fun current(): LoggingContext = coroutineContext[LoggingContext] ?: EMPTY
     }
 
+    /**
+     * Builder for constructing a `LoggingContext` object with optional configurations.
+     *
+     * This class allows you to configure a `LoggingContext` by adding a `Span` or a collection of `Tags`.
+     * The configurations are accumulated and applied in the final `build()` method, which produces
+     * an instance of `LoggingContext`.
+     */
     class Builder {
         private var span: Span? = null
         private var tags: MutableTags = mutableMapOf()
@@ -68,14 +102,17 @@ class LoggingContext(
         fun build(): LoggingContext = span?.let { LoggingContext(tags, it) } ?: LoggingContext(tags)
     }
 
+    /**
+     * A simple stack data structure to manage `Span` objects, allowing push, pop, and peek operations.
+     * Provides functionalities to add a new span to the stack, remove the most recently added span,
+     * or view it without removing.
+     */
     class SpanStack() {
         private val stack = mutableListOf<Span>()
         fun push(span: Span) = stack.add(span)
         fun pop(): Span? = stack.removeLastOrNull()
         fun peek(): Span? = stack.lastOrNull()
-        fun isEmpty(): Boolean = stack.isEmpty()
-        fun isNotEmpty(): Boolean = stack.isNotEmpty()
-        fun clear() = stack.clear()
+        fun current(): Span? = peek()
         override fun toString(): String = stack.joinToString(separator = ", ", prefix = "[", postfix = "]") { it.name }
     }
 }
